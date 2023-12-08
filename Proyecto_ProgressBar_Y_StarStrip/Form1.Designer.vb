74e5bdd648338beccc618c6c58e1c349457d5303
y1s1s <yisusnl420@gmail.com>
2023-12-08 21:07:23 +0100

    Add project files.

---
+++ Proyecto_ProgressBar_Y_StarStrip/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        ProgressBar1 = New ProgressBar()
+        CheckedListBox1 = New CheckedListBox()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        ProgressBar1.Location = New Point(253, 242)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(236, 23)
+        ProgressBar1.TabIndex = 0
+        ' 
+        ' CheckedListBox1
+        ' 
+        CheckedListBox1.FormattingEnabled = True
+        CheckedListBox1.Items.AddRange(New Object() {"Eres Programador?", "Eres Alcoholico?", "Eres Drogadicto?", "Eres calvo (o te estas quedando)?"})
+        CheckedListBox1.Location = New Point(253, 115)
+        CheckedListBox1.Name = "CheckedListBox1"
+        CheckedListBox1.Size = New Size(236, 94)
+        CheckedListBox1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(CheckedListBox1)
+        Controls.Add(ProgressBar1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Proyecto_ProgressBar_Y_StarStrip/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'vamos a inicializar el progressbar con la barra de progreso completa al principio
+
+        ProgressBar1.Value = 100
+
+
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox1.SelectedIndexChanged
+
+        'En el caso de clickar en el primer elemento de la lista, el progressbar se ira vaciando en 1 unidad por segundo
+
+        If CheckedListBox1.SelectedIndex = 0 Then
+
+            Timer1.Interval = 1000
+
+            Timer1.Start()
+
+        End If
+
+        'En el caso de que se clicke en el segundo de la lista la progressbar se ira vaciando en 2 unidades por segundo
+
+        If CheckedListBox1.SelectedIndex = 1 Then
+
+            Timer1.Interval = 500
+
+            Timer1.Start()
+
+        End If
+
+        'En el caso de que se clicke en el tercero de la lista la progressbar se ira vaciando en 3 unidades por segundo
+
+        If CheckedListBox1.SelectedIndex = 2 Then
+
+            Timer1.Interval = 333
+
+            Timer1.Start()
+
+        End If
+
+        'En el caso de que se clicke en el cuarto de la lista la progressbar se ira vaciando en 4 unidades por segundo
+
+        If CheckedListBox1.SelectedIndex = 3 Then
+
+            Timer1.Interval = 250
+
+            Timer1.Start()
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub timer1_tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'En el caso de que el progressbar llegue a 0, se parara el timer y se mostrara un mensaje
+
+        If ProgressBar1.Value = 0 Then
+
+            Timer1.Stop()
+
+            MsgBox("La barra de progreso se ha vaciado")
+
+        Else
+
+            'En el caso de que el progressbar no llegue a 0, se ira vaciando en 1 unidad por segundo
+
+            ProgressBar1.Value = ProgressBar1.Value - 1
+
+        End If
+
+    End Sub
+
+
+End Class
